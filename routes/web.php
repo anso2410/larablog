64d9561363ca1e26325a215d8451215eef5dc16f
anso2410 <simana.annesophie@gmail.com>
2021-11-22 12:56:39 +0100

    UserController.php:bind the controller userController to the route profile & add parameter username to the route profile

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\{
+    UserController
+};
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('profile', [UserController::class, 'profile'])->name('user.profile');
+
 
 Route::get('/', function () {
     return view('welcome');
