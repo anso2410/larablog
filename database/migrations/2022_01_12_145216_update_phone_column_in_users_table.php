fa214308367c952fe05bc48eaddb81db36fc95d0
anso2410 <simana.annesophie@gmail.com>
2022-01-12 16:05:54 +0100

    update varchar column phone in table users from 191 to 15

--- database/migrations/2022_01_12_145216_update_phone_column_in_users_table.php
+++ database/migrations/2022_01_12_145216_update_phone_column_in_users_table.php
@@ -14,7 +14,7 @@ class UpdatePhoneColumnInUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('phone', 15)->nullable()->change();
         });
     }
 
@@ -26,7 +26,7 @@ class UpdatePhoneColumnInUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('phone')->change();
         });
     }
 }
