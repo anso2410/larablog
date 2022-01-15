d7e41d2a1df8a2af6b2134f903a746d2445c3a67
anso2410 <simana.annesophie@gmail.com>
2022-01-15 18:42:09 +0100

    create css file

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,20 +10,7 @@
   <title>{{ $title ?? '' }}</title>
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
-
-  <style>
-    body {
-      padding-top: 56px;
-    }
-    h1.title{
-      font-size: 1.25rem;
-    }
-    div.error {
-      color: #f55;
-    }
-  </style>
-
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
