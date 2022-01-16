ea999fb81f6b44c5f726eacbf97d30f87bc78cd7
anso2410 <simana.annesophie@gmail.com>
2022-01-16 15:50:43 +0100

    register.blade:change delete style registration user success message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,7 +11,7 @@
         <div class="col-lg-9">
 
             @if (session('success'))
-                <div class="alert alert-success mt-3 text-center">
+                <div class="alert alert-success ">
                     {{ session('success') }}
                 </div>
             @endif
