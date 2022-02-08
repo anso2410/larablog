34f5bc1197e23ff3cf23a1eab586030dc6fddddd
anso2410 <simana.annesophie@gmail.com>
2022-02-08 15:37:40 +0100

    ForgotController:create function store to verify data and send link through email

--- app/Http/Controllers/ForgotController.php
+++ app/Http/Controllers/ForgotController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use  Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class ForgotController extends Controller
 {
@@ -21,7 +23,17 @@ class ForgotController extends Controller
  
     public function store() //verification des data et envoie de lien par mail 
     {
+        request()->validate([
+            'email'=>'required|email|exists:users',
+        ]);
 
+        $token = Str::uuid();
+
+        DB::table('password_resets')->insert([
+            'email'=>request('email'),
+            'token'=>$token,
+            'created_at'=>now(),
+        ]);
     }
 
 }
