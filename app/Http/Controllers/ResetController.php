8298cb8c835a5109b569f0a54c1903f71e646716
anso2410 <simana.annesophie@gmail.com>
2022-02-09 21:52:17 +0100

    ResetController:add query builder for password_resets table

--- app/Http/Controllers/ResetController.php
+++ app/Http/Controllers/ResetController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class ResetController extends Controller
 {
     public function index(string $token) //formulaire de réinitialisation de mdp
     {
-
+        $password_reset = DB::table('password_resets')->where('token', $token)->first();
+        dd($password_reset);
     } 
 }
