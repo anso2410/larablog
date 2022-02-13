95521131dff925f523aabdb149703671995fcc3e
anso2410 <simana.annesophie@gmail.com>
2022-02-13 18:40:00 +0100

    Article.php: add function to return slug in url instead of id

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,11 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-// relation entre article et le user: un article appartient à un utilisateur.
+    // relation entre article et le user: un article appartient à un utilisateur.
 
     use HasFactory;
+    
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class); // un article appartient à un utilisateur
     }
 }
