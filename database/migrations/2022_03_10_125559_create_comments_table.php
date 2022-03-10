1824f61dc0e068adcd3355871d981cdec730bbb2
anso2410 <simana.annesophie@gmail.com>
2022-03-10 20:43:57 +0100

    nothing to commit

--- database/migrations/2022_03_10_125559_create_comments_table.php
+++ database/migrations/2022_03_10_125559_create_comments_table.php
@@ -15,7 +15,7 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('article_id')->constrained()->onDelete('cascade'); cbcvdgteghfyjguifkf
+            $table->foreignId('article_id')->constrained()->onDelete('cascade'); 
             $table->timestamps();
         });
     }
