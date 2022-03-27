3e569add314036f9ff6ee7fb8fb8cfadac68377a
anso2410 <simana.annesophie@gmail.com>
2022-03-27 17:00:16 +0200

    CommentController:add requierement  to send email if the one who comment is not the author of the article

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\CommentRequest as RequestsCommentRequest;
+use App\Notifications\NewComment;
 
-use App\Models\{Comment,Article
+use App\Models\{
+    Comment,
+    Article
 };
 //use App\Htpp\Requests\CommentRequest;
 
@@ -22,11 +25,15 @@ class CommentController extends Controller
         $validatedData = $request->validated();
         $validatedData['user_id'] = auth()->id();
 
-        $article->comments()->create($validatedData);
+        $comment =  $article->comments()->create($validatedData);
+
+        if (auth()->id() !== $article->user_id) // si le commentateur n'est l'auteur de l'article
+        {
+            $article->user->notify(new NewComment($comment));
+        }
 
         $success = 'Commentaire ajouté avec succès.';
 
         return back()->withSuccess($success);
-
     }
 }
