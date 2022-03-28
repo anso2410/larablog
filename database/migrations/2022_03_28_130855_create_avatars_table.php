5c9d49558f7923c5b21f0f1d1a21bfd5ab40cbe1
anso2410 <simana.annesophie@gmail.com>
2022-03-28 15:26:00 +0200

    migrations/avatar:create database's field user_id with foreign key id user & add constrains for deleting all field in avatar's table if user delete profile

--- database/migrations/2022_03_28_130855_create_avatars_table.php
+++ database/migrations/2022_03_28_130855_create_avatars_table.php
@@ -15,6 +15,7 @@ class CreateAvatarsTable extends Migration
     {
         Schema::create('avatars', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
