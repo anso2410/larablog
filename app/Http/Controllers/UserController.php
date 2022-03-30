47a7b5f71339a97818884c305f0eb82827a8ea0e
anso2410 <simana.annesophie@gmail.com>
2022-03-30 14:25:04 +0200

    userController:update user's informations in database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,9 +8,7 @@ use Illuminate\Support\Str;
 use Intervention\Image\Facades\Image;
 use Illuminate\Support\Facades\Storage;
 
-use App\Models\User;
-
-;
+use App\Models\User;;
 
 class UserController extends Controller
 {
@@ -18,10 +16,10 @@ class UserController extends Controller
     {
         $this->middleware('auth')->except('profile');
     }
-    
+
     public function profile(User $user)
     {
-        return 'Je suis un utilisateur et mon nom est '.$user->name;
+        return 'Je suis un utilisateur et mon nom est ' . $user->name;
     }
 
     public function edit()
@@ -36,33 +34,30 @@ class UserController extends Controller
     }
 
     public function store()
-    {   
+    {
         $user = auth()->user();
+       $user = $user->updateOrCreate(['id'=>$user->id],
         request()->validate([
             'name' => ['required', 'min:3', 'max:20', Rule::unique('users')->ignore($user)],
             'email' => ['required', 'email', Rule::unique('users')->ignore($user)],
-            'avatar' => ['sometimes', 'nullable', 'image', 'file', 'mimes:jpeg,png'],
-        ]);
+            'avatar' => ['sometimes', 'nullable', 'image', 'file', 'mimes:jpeg,png', 'dimensions:min_width=200,min_height=200'],
+        ]));
+        
 
-        if(request()->hasFile('avatar') && request()->file('avatar')->isValid())
-        {   
+        if (request()->hasFile('avatar') && request()->file('avatar')->isValid()) {
             $ext = request()->file('avatar')->extension();
-            $filename = Str::slug($user->name).'-'.$user->id.'.'.$ext;
-            
-            $path = request()->file('avatar')->storeAs('avatars/'.$user->id, $filename);
+            $filename = Str::slug($user->name) . '-' . $user->id . '.' . $ext;
 
-             $thumbnailImage = Image::make(request()->file('avatar'))->fit(200, 200, function($constraint)
-             {
+            $path = request()->file('avatar')->storeAs('avatars/' . $user->id, $filename);
+
+            $thumbnailImage = Image::make(request()->file('avatar'))->fit(200, 200, function ($constraint) {
+
                 $constraint->upsize();
-                
-             })->encode($ext, 50);
+            })->encode($ext, 50);
 
-             $thumbnailPath = 'avatars/'.$user->id.'/thumbnail/'.$filename;
+            $thumbnailPath = 'avatars/' . $user->id . '/thumbnail/' . $filename;
 
-             Storage::put($thumbnailPath, $thumbnailImage);
-            
-            
-        }   
-
+            Storage::put($thumbnailPath, $thumbnailImage);
+        }
     }
 }
