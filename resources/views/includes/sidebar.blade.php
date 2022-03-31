e51bae929ba758bd21a7368569cbffac4e1c2d83
anso2410 <simana.annesophie@gmail.com>
2022-03-31 17:29:02 +0200

    fix syntax error

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -2,6 +2,6 @@
 <div class="list-group">
 
 @foreach(App\Models\Category::has('articles')->get() as $category)
-<a href="{{ route('category.show', ['category'=>slug]) }}" class="list-group-item">{{ $category->name }}</a>
+<a href="{{ route('category.show', ['category'=>$category->slug]) }}" class="list-group-item">{{ $category->name }}</a>
 @endforeach
 </div>
