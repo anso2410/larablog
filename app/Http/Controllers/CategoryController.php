6e559d63c3e6cc09e1fc8bc02cadd4a0dbfbb6d1
anso2410 <simana.annesophie@gmail.com>
2022-03-31 17:22:23 +0200

    create public function show  &add use for models category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+    public function show(Category $category) //les articles de la catégorie
+    {
+        dd($category);
+    }
 }
