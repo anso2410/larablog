7f6a1dfa2e34fbe3e9f756f9090bf90fc5a6c509
anso2410 <simana.annesophie@gmail.com>
2022-03-31 17:13:24 +0200

    use method has to not display category if its has no article

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="my-4">Catégories</div>
 <div class="list-group">
 
-@foreach(App\Models\Category::get() as $category)
+@foreach(App\Models\Category::has('articles')->get() as $category)
 <a href="#" class="list-group-item">{{ $category->name }}</a>
 @endforeach
 </div>
